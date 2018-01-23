3e3b3fd59d51ce3eb6a2748eef0dd8d8ed9bd0e6
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-01-22 23:19:57 -0500

    mobile detection conditioning added to the default.js file

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,5 +1,11 @@
-var winH = window.innerHeight;
-$('.bcontent').css('height',(79/100 * winH)+'px');
+//detecting if site is on mobile device 
+if( !navigator.userAgent.match(/Android/i) || !navigator.userAgent.match(/webOS/i) || !navigator.userAgent.match(/iPhone/i) || !navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i)
+ || !navigator.userAgent.match(/BlackBerry/i)
+ || !navigator.userAgent.match(/Windows Phone/i)
+ {
+    var winH = window.innerHeight;
+    $('.bcontent').css('height',(79/100 * winH)+'px');
+ }
 
 $('button').hover(function(){
 	$(this).css('background-color','black');
