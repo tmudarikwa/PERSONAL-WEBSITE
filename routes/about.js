ff40436e10cb02e97c98cde45b1ec4743676daf7
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-01-23 00:22:00 -0500

    new route testing

--- routes/about.js
+++ routes/about.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var date = new Date();
-
+console.log("---router called---");
 /* GET home page. */
 router.get('/about', function(req, res, next) {
+  console.log('---path matched---');
   res.render('about', { title: 'Takunda Mudarikwa | About', year: date.getFullYear()});
 });
 
