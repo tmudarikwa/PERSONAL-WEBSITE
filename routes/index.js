92fa51a66f99a95001fa60d216637a79d6bf84ad
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-01-23 20:33:43 -0500

    added home button on layout and changed some styles

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var date = new Date();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var path = '/';
+  res.locals.path = path;
   res.render('index', { title: 'Takunda Mudarikwa | HOME', year: date.getFullYear()});
 });
 
