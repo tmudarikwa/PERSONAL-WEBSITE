a3ab45b65cb56ec87e6c8af6352a6e57829a8135
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-01-24 02:02:20 -0500

    email config folder and file ; noty plugin and contactemail route

---
+++ config/mandrillemail.js
@@ -0,0 +1,68 @@
+var mandrill = require('mandrill-api/mandrill');
+
+
+var mandrill_client = new mandrill.Mandrill('lQi6Z4MbRPDIZ7P8M2qAxg');
+
+var sendcontactemail = function(req, res){
+
+     var data = req.body;
+
+      var message = {
+                    "html": "<p>"+data.message+"</p>",
+                    "text": "Example text content",
+                    "subject": "TAKUNDA MUDARIKWA PERSONAL WEBSITE NEW MESSAGE",
+                    "from_email": "contactus@gerfusa.com",
+                    "from_name": ""+data.email+"",
+                    "to": [{
+                            "email": "tkmud91@hotmail.com",
+                            "name": "GERF - CONTACTUS",
+                            "type": "to"
+                        }],
+                    "headers": {
+                        "Reply-To": ""+data.email+""
+                    },
+                    "important": false,
+                    "track_opens": null,
+                    "track_clicks": null,
+                    "auto_text": null,
+                    "auto_html": null,
+                    "inline_css": null,
+                    "url_strip_qs": null,
+                    "preserve_recipients": null,
+                    "view_content_link": null,
+                    "bcc_address": ""+data.email+"",
+                    "tracking_domain": null,
+                    "signing_domain": null,
+                    "return_path_domain": null,
+                    "merge": true,
+                    "merge_language": "mailchimp",
+                    "global_merge_vars": [{
+                            "name": "merge1",
+                            "content": "merge1 content"
+                        }],
+                    "merge_vars": [{
+                            "rcpt": "recipient.email@example.com",
+                            "vars": [{
+                                    "name": "merge2",
+                                    "content": "merge2 content"
+                                }]
+                        }]
+                };
+      var async = false;
+      var ip_pool = "Main Pool";
+      //var send_at = "example send_at";
+      mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool}, function(result) {
+        var emailsent = "Information submitted! Our team will get back to you asap. Thank you.";
+        if(result[0].status !== 'sent') emailsent = "We apologize there has been an error trying to submit your information.";
+        res.send(emailsent);
+      }, function(e) {
+          // Mandrill returns the error as an object with name and message keys
+          var emailsent = "We apologize there has been an error trying to submit your information.";
+          res.send(emailsent);
+          // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+      });
+}
+
+
+
+module.exports.sendContactEmail = sendcontactemail;
---
+++ routes/contactemail.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var sendcontactemail = require('../config/mandrillemail');
+
+// =====================================
+// EMAIL SENT FROM THE CONTACT US PAGE==
+// =====================================
+router.post('/contactusemail', function(req,res){
+    sendcontactemail.sendContactEmail(req,res);
+});
+
+module.exports = router;
