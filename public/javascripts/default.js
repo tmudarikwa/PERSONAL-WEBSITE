b88d89d6b7c47ad9cf9cdec5236c63d6b218ae0e
tmudarikwa <tkmud91@hotmail.com>
2018-03-14 02:41:58 -0400

    mobile image width resolve

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,6 +1,6 @@
 var winH = window.innerHeight;
 //detecting if site is on mobile device 
-if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i)
+if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)
  || navigator.userAgent.match(/Windows Phone/i))
  {
