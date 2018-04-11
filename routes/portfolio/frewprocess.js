f5ff9a9a432e64225dd45da4df7c46c5d7b5ec3e
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-04-11 00:36:36 -0400

    adding frewprocess portfolio portal path fix

--- routes/portfolio/frewprocess.js
+++ routes/portfolio/frewprocess.js
@@ -6,6 +6,7 @@ var date = new Date();
 /* GET portifolio page. */
 router.get('/', function(req, res, next) {
   var path = '/portfolio/frewprocess';
+  console.log('*******path**************');
   console.log(path);
   res.locals.path = path;
   res.render('portfolio/frewprocess', { title: 'Takunda Mudarikwa - Full-stack Web Developer | Portfolio - Frewprocess Portal', year: date.getFullYear()});
