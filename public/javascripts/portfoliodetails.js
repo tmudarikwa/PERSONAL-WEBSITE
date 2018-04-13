814f8608b4507b5f932a4d7bd135c5044931664a
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-04-13 03:28:25 -0400

    carousel removal

--- public/javascripts/portfoliodetails.js
+++ public/javascripts/portfoliodetails.js
@@ -5,6 +5,6 @@ $('#portfoliodetails').children('.active').show();
 //navigation through a selected projects details (when a user clicks on a projects image whilst on /portfolio)
 $('.portfoliodetail button').click(function(){
   var direction = $(this).text();
-  var currentItem = $(this).children('.active').attr('id');
+  var currentItem = $('#portfoliodetails').children('.active').attr('id');
   console.log(currentItem);
 });
