b505f639f51ff00f24a40ef244e7c35d37bff477
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-04-15 02:02:06 -0400

    resume path name change

--- routes/viewresume.js
+++ routes/viewresume.js
@@ -6,14 +6,14 @@ var path = require('path');
 var date = new Date();
 /* download resume copy */
 router.get('/', function(req, res, next) {
-  var vpath = '/viewresume';
-  res.locals.path = vpath;	
+  var vpath = '/TAKUNDAEUGENEMUDARIKWA-RESUME';
+  res.locals.path = vpath;
   var file = path.join(__dirname, '../public/resume/TAKUNDA EUGENE MUDARIKWA.pdf');
-  
+
   fs.readFile(file, function(err,data){
     res.contentType("application/pdf");
     res.send(data);
-  }); 
+  });
 });
 
 
