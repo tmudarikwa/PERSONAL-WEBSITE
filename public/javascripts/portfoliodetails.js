413d360fb46b884538d31299625b869334509b98
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-04-17 05:26:07 -0400

    added expense allocation metrics details page

--- public/javascripts/portfoliodetails.js
+++ public/javascripts/portfoliodetails.js
@@ -21,7 +21,9 @@ $('.portfoliodetail button').click(function(){
   var numOfItems = $('#portfoliodetails').children().length;
 
   //displaying next or prev item
-  if(checkNextItem == 1 && nextItem > 1 && nextItem < numOfItems && direction.startsWith('NEXT')){
+  if(numOfItems <= 1 ){
+    $("#nextportfoliobtn").hide();
+  }else if(checkNextItem == 1 && nextItem > 1 && nextItem < numOfItems && direction.startsWith('NEXT')){
     $("#portfoliodetails #"+nextItem+"").addClass('active');
     $("#previousportfoliobtn").show();
   }else if(checkNextItem == 1 && nextItem == numOfItems && direction.startsWith('NEXT')){
