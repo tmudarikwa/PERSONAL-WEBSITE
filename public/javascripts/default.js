ee632b1ab49eba9c012f381a41a200b2ff6763d9
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-04-19 21:34:40 -0400

    added an image for the back button and the functional implementation

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -28,6 +28,7 @@ if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i)
       }
     );
    $('#portfoliodetails .content').show();
+   $('.portfoliodetail .col-1 img').css('width','2.5rem');
 
  }else{
 
