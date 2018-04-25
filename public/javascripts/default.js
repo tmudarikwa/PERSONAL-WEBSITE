a92d553b733bbdcac8cb985b07f305707aedf1cf
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-04-24 22:14:28 -0400

    updated footer styling

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -29,7 +29,9 @@ if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i)
     );
    $('#portfoliodetails .content').show();
    $('.portfoliodetail .col-1 img').css('width','2.5rem');
-
+   //setting footer Height
+   var footerHeight = winH - ($(".portfolio")[0].clientHeight + $("nav")[0].clientHeight);
+   $('.footer').css('height',footerHeight+'px');
  }else{
 
     //setting main body content height
