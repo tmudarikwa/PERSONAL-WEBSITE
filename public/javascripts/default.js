6a7cd97515e9f330bb80fd08f233c4a844d70d77
Takunda Mudarikwa <tkmud91@hotmail.com>
2018-07-09 19:52:52 -0400

    index profile image change and some overal minor css changes to adhere with the new look

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -3,6 +3,8 @@ var winH = window.innerHeight;
 if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)
  || navigator.userAgent.match(/Windows Phone/i)){
+    $('body').css("height","100%");
+    $('.profilepic').css("height","300px");
     $('nav').addClass("sticky-top");
     $('.navlink').css("margin","0");
     $('.navlink').css("padding","0");
