e383332d403d3c46e6fe30ad08f026c9345998b4
tmudarikwa <tkmud91@hotmail.com>
2021-04-14 12:59:54 -0400

    added a new section for my technical skills

---
+++ public/javascripts/skills.js
@@ -0,0 +1,80 @@
+//const d3 = require("d3");
+function skillsGraph(id){
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 120, bottom: 40, left: 120},
+        width = document.body.clientWidth - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(id)
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Parse the Data
+    d3.csv("/javascripts/skills_data.csv", function(data) {
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([1, 6])
+        .range([ 0, width]);
+    svg.append("g")
+        .text("Years of Experience")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Y axis
+    var y = d3.scaleBand()
+        .range([ 0, height ])
+        .domain(data.map(function(d) { return d.skills; }))
+        .padding(1);
+    svg.append("g")
+        .call(d3.axisLeft(y))
+
+    var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return  "<strong><span style='color:#2c3e50;font-size:9.5px;margin-left:25px;'>"+d.years+"</span></strong>"; });
+
+    svg.call(tip);
+
+    // Lines
+    svg.selectAll("myline")
+    .data(data)
+    .enter()
+    .append("line")
+
+    // Circles
+    svg.selectAll("mycircle")
+    .data(data)
+    .enter()
+    .append("circle")
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+
+    // Animation
+    svg.selectAll("line")
+        .transition()
+        .duration(1600)
+        .attr("x1", function(d) { return x(d.years); })
+        .attr("x2", x(0))
+        .attr("y1", function(d) { return y(d.skills); })
+        .attr("y2", function(d) { return y(d.skills); })
+        .attr("stroke", "rgb(152 180 208)")
+        .delay(function(d,i){; return(i*300)})
+
+    svg.selectAll("circle")
+        .transition()
+        .duration(1000)
+        .attr("cx", function(d) { return x(d.years); })
+        .attr("cy", function(d) { return y(d.skills); })
+        .attr("r", "7")
+        .style("fill", "rgb(152 180 208)")
+        .attr("stroke", "#2c3e50")
+        .delay(function(d,i){; return(i*200)})
+
+    })
+}
