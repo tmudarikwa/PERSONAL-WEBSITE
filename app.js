55cd31cfba2c9ba644cfe1c23cf6fc7e1ba1e9d4
Trevor <nymud94@gmail.com>
2021-08-31 21:56:22 +0200

    changed corse origin

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: 'https://pixykorner.co.za'
+  origin: 'https://redwaste.africa'
 }));
 
 app.use('/', index);
