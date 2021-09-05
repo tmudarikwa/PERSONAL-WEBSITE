0e2974595d0f9254ecc1c4566fdfe7e9a53cf426
tmudarikwa <tkmud91@hotmail.com>
2021-09-05 12:31:06 -0400

    added Redwaste confirmation email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -61,7 +61,7 @@ let sendContactEmailRedWaste =  function(req, res){
                   subject: "REDWASTE EMAIL",
                   from_email: data.email,
                   from_name: "RedWaste",
-                  to:"nymud94@gmail.com",//"info@redwaste.africa",
+                  to:"info@redwaste.africa",
               };
       //var send_at = "example send_at";
       transporter.sendMail(message, function(error,result) {
@@ -128,7 +128,7 @@ let sendOrderEmailRedWaste =  function(req, res){
                   subject: "REDWASTE Order Email",
                   from_email: data.email,
                   from_name: "RedWaste",
-                  to:"nymud94@gmail.com",//"info@redwaste.africa",
+                  to:"info@redwaste.africa",
               };
       //var send_at = "example send_at";
       transporter.sendMail(message, function(error,result) {
@@ -191,8 +191,8 @@ let sendConfirmationEmailRedWaste =  function(req, res){
 								"</div>\n"+
 							"</div>\n"+
 						"</div>\n"+
-						"<div style='width:100;font-weight:bold;color:black !important;background:white;margin-top:4rem'>\n"+
-							"<p style='float:left'><br/><br/>Regards,<br/><br/> RedWaste Team </p>\n"+
+						"<div style='width:100;font-weight:bold;color:black !important;background:white;margin-top:2rem'>\n"+
+							"<p style='float:left'>Regards,<br/><br/> RedWaste Team </p>\n"+
 						"</div>\n";
 
     var message = {
