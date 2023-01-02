773cd17bdb20318f3fa5093a3a5385fc8e6b13ad
tmudarikwa <tkmud91@hotmail.com>
2023-01-02 17:10:19 -0500

    fixed file open error

--- config/roundTable.js
+++ config/roundTable.js
@@ -1,27 +1,24 @@
 const fs = require('fs');
 
-let tableDetails = [];
-fs.readFile('config/roundtable.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  let rawTableDetails = null;
-  if (data.search(';') != -1){
-    rawTableDetails = data.split(";")
-  }else{
-    rawTableDetails = data.split(",");
-  }
-  
-  rawTableDetails.forEach(aTableDetail => {
-    tableDetails.push(aTableDetail.replace("\r\n",""));
-  });
-
-});
-
 let getDetails = () => {
-    
+    let tableDetails = [];
+    let data = null;
+    try {
+        data = fs.readFileSync('config/roundtable.txt', 'utf8');
+    } catch (err) {
+        console.log(err);
+    }
+
+    let rawTableDetails = [];
+    if (data.search(';') != -1){
+        rawTableDetails = data.split(";")
+    }else{
+        rawTableDetails = data.split(",");
+    }
+    rawTableDetails.forEach(aTableDetail => {
+        tableDetails.push(aTableDetail.replace("\r\n",""));
+    });
+
     let detailsEntriesArray = [];
     tableDetails.forEach(row =>{
         let rowArray = row.split("|");
@@ -32,7 +29,9 @@ let getDetails = () => {
             name: rowArray[3],
             amount: rowArray[4]
         });
-    })
+    });
+
+    console.log(tableDetails);
 
     const details = {
                         detailsEntries: detailsEntriesArray
